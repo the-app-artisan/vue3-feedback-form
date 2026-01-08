da1f19491b6a383856a663a49c56a817884bf680
The App Artisan <theappartisan.dev@gmail.com>
2026-01-08 13:45:50 +0400

    Replace HelloWorld with FeedbackForm component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
+import FeedbackForm from './components/FeedbackForm.vue';
 </script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <FeedbackForm />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>

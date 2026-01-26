03b19c22df10e9a1872a95d3c975236f9aca07f3
The App Artisan <theappartisan.dev@gmail.com>
2026-01-26 12:39:14 +0400

    Extract shared card styles into FeedbackCard

---
+++ src/components/FeedbackCard.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-white p-8 rounded-lg shadow-md">
+    <slot></slot>
+  </div>
+</template>
--- src/components/FeedbackSuccess.vue
+++ src/components/FeedbackSuccess.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
 import { CheckIcon } from '@heroicons/vue/16/solid';
+import FeedbackCard from './FeedbackCard.vue';
 </script>
 
 <template>
-  <div
-    class="bg-white p-8 rounded-lg shadow-md min-h-72 flex flex-col items-center justify-center"
-  >
-    <CheckIcon class="text-indigo-600 w-16 h-16" />
-    <h2 class="text-xl text-center font-semibold text-gray-900">
-      Thank you for your feedback!
-    </h2>
-    <p class="mt-1 text-sm/6 text-center text-gray-600">
-      We appreciate your input and will use it to improve our service
-    </p>
-  </div>
+  <FeedbackCard>
+    <div class="min-h-72 flex flex-col items-center justify-center">
+      <CheckIcon class="text-indigo-600 w-16 h-16" />
+      <h2 class="text-xl text-center font-semibold text-gray-900">
+        Thank you for your feedback!
+      </h2>
+      <p class="mt-1 text-sm/6 text-center text-gray-600">
+        We appreciate your input and will use it to improve our service
+      </p>
+    </div>
+  </FeedbackCard>
 </template>

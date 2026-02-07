79187a7c3c3c87ef02d87ded89dc57c4847df999
The App Artisan <theappartisan.dev@gmail.com>
2026-02-07 09:28:03 +0400

    Add TypeScript support to FeedbackForm component

--- src/components/FeedbackForm.vue
+++ src/components/FeedbackForm.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script lang="ts" setup>
 import { ChevronDownIcon } from '@heroicons/vue/16/solid';
 import FeedbackSuccess from './FeedbackSuccess.vue';
 import FeedbackCard from './FeedbackCard.vue';
